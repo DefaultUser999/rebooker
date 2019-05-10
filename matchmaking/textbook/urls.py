5f667c904a04969270acf1744a574d3a8000b341
jefflee0510 <jeffreylee0510@gmail.com>
2019-05-10 15:23:22 +1000

    Django integration

--- matchmaking/textbook/urls.py
+++ matchmaking/textbook/urls.py
@@ -1,7 +1,16 @@
-# users/urls.py
+# textbook/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('about/', views.About, name='about'),
+    path('cart/', views.Cart, name='cart'),
+    path('checkout/', views.Checkout, name='checkout'),
+    path('contact/', views.Contact, name='contact'),
+    path('featured/', views.Featured, name='featured'),
+    path('index/', views.Index, name='index'),
+    path('order-complete/', views.OrderComplete, name='order-complete'),
+    path('product-detail/', views.ProductDetail, name='product-detail')
+    # path('buy/', views.buy, name='buy'),
 ]
--- matchmaking/textbook/views.py
+++ matchmaking/textbook/views.py
@@ -13,3 +13,42 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+    
+def About(request):
+    template_name = 'about.html'
+    return render(request, 'about.html')
+    
+def Cart(request):
+    template_name = 'cart.html'
+    return render(request, 'cart.html')
+    
+def Checkout(request):
+    template_name = 'checkout.html'
+    return render(request, 'checkout.html')
+    
+def Contact(request):
+    template_name = 'contact.html'
+    return render(request, 'contact.html')
+    
+def Featured(request):
+    template_name = 'featured.html'
+    return render(request, 'featured.html')
+    
+def Index(request):
+    template_name = 'index.html'
+    return render(request, 'index.html')
+
+def OrderComplete(request):
+    template_name = 'order-complete.html'
+    return render(request, 'order-complete.html')
+    
+def ProductDetail(request):
+    template_name = 'product-detail.html'
+    return render(request, 'product-detail.html')
+    
+    
+    
+    
+    
+    
+    
