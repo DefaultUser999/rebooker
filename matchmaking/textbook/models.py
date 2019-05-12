3ebf9a633492df44cd7408fd656aa7dd9cbcf8a0
shawn taylor <shawn.btaylor4@gmail.com>
2019-05-12 19:17:35 +1000

    updated item model to better handle buying

--- matchmaking/textbook/models.py
+++ matchmaking/textbook/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
 
 class CustomUser(AbstractUser):
     # add additional fields in here
@@ -9,20 +10,26 @@ class CustomUser(AbstractUser):
         return self.email
 
 class Item(models.Model):
-    seller = models.ForeignKey(
+    seller = models.OneToOneField(
         get_user_model(),
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='user_selling_item'
     )
-    sell_date = models.DateTimeField( auto_now=True, auto_now_add=True)
+    sell_date = models.DateTimeField(auto_now_add=True)
     # buy_date
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     status_sold = models.BooleanField(default=False) # used to check if textbook is sold or not : true == sold
-    # buyer = models.ForeignKey(
-    #     get_user_model(),
-    #     on_delete=models.CASCADE
-    # )
+    buyer = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        # primary_key=False
+        null=True,
+        related_name='user_buying_item'
+    )
     # title = models.CharField(max_length=100)
     # description = models.TextField(blank=True, null=True)
     # featured    = models.BooleanField(default=False) # null=True, default=True
+    def print_info(self):
+        return "seller: " + self.seller + "Buyer: " + self.buyer
