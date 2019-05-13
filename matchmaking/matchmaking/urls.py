033013ad182062ebac35e6eb30a41283f52a2dc1
Elwyn <s3629491@student.rmit.edu.au>
2019-05-13 13:27:30 +1000

    Fixed urls bugs

--- matchmaking/matchmaking/urls.py
+++ matchmaking/matchmaking/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
-    path('textbook/', include('textbook.urls')),
+    path('', include('textbook.urls')),
     path('users/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
