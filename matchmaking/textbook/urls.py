17fdac9d82335d43207fea29db592fe81059d287
Elwyn <s3629491@student.rmit.edu.au>
2019-05-13 13:20:45 +1000

    Fixed urls bugs

--- matchmaking/textbook/urls.py
+++ matchmaking/textbook/urls.py
@@ -1,11 +1,17 @@
 # users/urls.py
-from django.urls import path
+#Reference https://academy.muva.tech/blog/?s=Django+2.
+from django.urls import path, re_path
 from . import views
 
+
+app_name='textbook'
+
 urlpatterns = [
+    path('product/detail/<int:id>/', views.product_detail, name='detail'),
+    path('products/', views.products, name='products' ),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path(r'^$', views.product_list, name='product_list'),
-    path(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
-    path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+    re_path(r'^$', views.product_list, name='product_list'),
+    re_path(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
+    re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
 
 ]
